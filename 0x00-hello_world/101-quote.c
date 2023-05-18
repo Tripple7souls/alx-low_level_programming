49f4623f511663cced3b056431c1f9225247f98f
Tripple7souls <tripple7souls@gmail.com>
2023-05-18 22:34:08 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - write out the string
+ *
+ * Return: 1
+*/
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(char), 47, stdout);
+
+	return (1);
+}
+
