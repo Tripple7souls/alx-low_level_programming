96bd8f0938dbb2106d6dad24d876c496ec9a4c79
Tripple7souls <tripple7souls@gmail.com>
2023-05-18 23:05:08 +0000

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(char), 47, stdout);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
