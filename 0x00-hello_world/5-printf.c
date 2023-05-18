879c5d9ce91bbfac7b8f99296e81132d0102838d
Tripple7souls <tripple7souls@gmail.com>
2023-05-18 17:22:42 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print formatted output to the standard output stream
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
