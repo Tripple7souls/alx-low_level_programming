7506398c07a0f9ea6e30c1fb79d448efc93a6e2c
Tripple7souls <tripple7souls@gmail.com>
2023-05-26 07:42:40 +0000

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,21 +10,20 @@ int main(void)
 {
 	int a;
 	int b;
-	int count;
 
-	for (a = 0; a <= 8; a++)
-		for (b = a + 1; b <= 9; b++)
-			if (a == 0)
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
 		{
-			putchar('0' + a);
-		}
-			putchar('0' + b);
-			count++;
-			if (count < 45)
+			putchar(a);
+			putchar(b);
+			if (a < '8' && b <= '9')
 			{
-			putchar(',');
-			putchar(' ');
+				putchar(',');
+				putchar(' ');
 			}
+		}
+	}
 	putchar('\n');
 
 	return (0);
