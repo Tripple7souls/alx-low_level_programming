95afe85794a3bd10ed61e8fbf260191f7dafe672
Tripple7souls <tripple7souls@gmail.com>
2023-05-26 00:15:15 +0000

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,15 +18,15 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-
+		
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-	else if (month == 2 && day == 60)
+	else 
 	{
-		{
+			day++;
+			
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
+        }
 }
